d892cd33c23ea7f8d1bb65df1d6ce06be765c0c0
Namit Chopra <namitc.11@gmail.com>
2021-06-15 01:21:53 -0400

    added functionality that allows user to use to operate two numbers for which the result can be seen on the display

--- script.js
+++ script.js
@@ -1,21 +1,57 @@
 const display = document.querySelector(".screen");
 const keypad = document.querySelector(".buttons");
-let num = 0;
+// created the num as an array to make it easier to add and remove numbers
+let num = [];
+let expression = [];
+let memory = 0;
 
-// Getting the input value
+// Takes the button that is clicked by the user and performs the corresponding
+// function; as of now, only way to show to results is to click the equals btn
 keypad.addEventListener("click", e => {
-    let buttonPressed = e.target.className;
-    if(buttonPressed === "digit"){
-        num = e.target.textContent;
-        display.textContent = num;
+    let buttonPressed = e.target;
+    if(buttonPressed.className === "digit"){
+        if(buttonPressed.id === "decimal"){
+            num.push(".");
+            display.textContent = num.join("");
+        }
+        else{
+            num.push(buttonPressed.textContent);
+            display.textContent = num.join("");
+        }
     }
-    else if (buttonPressed === "clear"){
-        num = 0;
-        display.textContent = num;
+    else if(buttonPressed.className === "clear"){
+        // resetting the calculator values
+        num = [];
+        expression = []
+        memory = 0;
+        display.textContent = 0;
     }
-    else{
-        display.textContent = "None";
+    else if(buttonPressed.className.includes("operator")){
+        //buttonPressed.classList.add("operator-active");
+        // moving number from input to memory
+        if(num.length < 1){
+            expression.push(memory);
+        }
+        else{
+            expression.push(num.join(""));
+        }
+        expression.push(buttonPressed.textContent);
+        num = [];
+        console.log(expression)
     }
+    else if(buttonPressed.className === "equals"){
+        expression.push(num.join(""));  // adding the second input number
+        let answer = operate(parseFloat(expression[0]), 
+                            parseFloat(expression[2]), expression[1]);
+        console.log(answer);
+        display.textContent = answer;
+        // clearing the current expression and numbers
+        num = [];
+        expression = []
+        //storing only the previous answer if needed for the next calculation
+        memory = answer; 
+    }
+    // console.log(expression);
 });
 
 // Operations
@@ -37,19 +73,21 @@ const divide = (num1,num2) => {
 
 // Function that control which operation to call
 const operate = (num1, num2, operation) => {
+    console.log(num1, num2, operation);
+    let ans;
     switch(operation){
         case '+':
-            console.log(add(num1, num2));
+            ans = add(num1, num2);
             break;
         case '-':
-            console.log(subtract(num1, num2));
+            ans = subtract(num1, num2);
             break;
-        case '*':
-            console.log(multiply(num1, num2));
+        case 'x':
+            ans = multiply(num1, num2);
             break;
-        case '/':
-            console.log(divide(num1, num2));
+        case '÷':
+            ans = divide(num1, num2);
             break;
     }
-    return 0; 
+    return ans; 
 }
